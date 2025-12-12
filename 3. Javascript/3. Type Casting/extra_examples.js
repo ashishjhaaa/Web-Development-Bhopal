dd4dcc4f161237f9a76447aea0f824d0f1aa014d
Ashish Jha <ashish.jhaaaaa@gmail.com>
2025-12-13 01:33:45 +0530

    Create extra_examples.js for type casting practice
    Add extra examples for type conversion in JavaScript.

---
+++ 3. Javascript/3. Type Casting/extra_examples.js
@@ -0,0 +1,15 @@
+// Students practice more examples for clarity on type conversion
+
+console.log("'5' + 2 =", "5" + 2);
+console.log("'5' - 2 =", "5" - 2);
+
+console.log("true + 1 =", true + 1);
+console.log("false + 1 =", false + 1);
+
+console.log("null + 1 =", null + 1);
+console.log("undefined + 1 =", undefined + 1);
+
+console.log("Number(true) =", Number(true));
+console.log("Number(false) =", Number(false));
+console.log("Number(null) =", Number(null));
+console.log("Number(undefined) =", Number(undefined));
