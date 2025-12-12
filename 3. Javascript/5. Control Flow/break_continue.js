cb34287206a276957ed3605aa5161f9f98ffbb2b
Ashish Jha <ashish.jhaaaaa@gmail.com>
2025-12-13 02:16:20 +0530

    Add break and continue examples in JavaScript
    Added examples demonstrating the use of break and continue in loops.

---
+++ 3. Javascript/5. Control Flow/break_continue.js
@@ -0,0 +1,13 @@
+// break example
+
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) break;
+  console.log("break example:", i);
+}
+
+// continue example
+
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) continue;
+  console.log("continue example:", i);
+}
