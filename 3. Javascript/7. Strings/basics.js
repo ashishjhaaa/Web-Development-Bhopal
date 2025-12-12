07cae94781aa6bce2598b292350e20030525fb53
Ashish Jha <ashish.jhaaaaa@gmail.com>
2025-12-13 02:40:40 +0530

    Create basics.js

---
+++ 3. Javascript/7. Strings/basics.js
@@ -0,0 +1,9 @@
+// String basics & indexing
+
+let str = "Hello World";
+
+console.log("First char:", str[0]);
+console.log("Second char:", str[1]);
+console.log("Last char:", str[str.length - 1]);
+
+console.log("Length:", str.length);
